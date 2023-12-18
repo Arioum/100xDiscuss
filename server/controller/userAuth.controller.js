3515207eec5230199690103ff7a04aecd4ace0eb
arioum <amoghrao121@gmail.com>
2023-12-19 00:39:52 +0530

    implemented token verification

--- server/controller/userAuth.controller.js
+++ server/controller/userAuth.controller.js
@@ -4,29 +4,58 @@ const jwt = require('jsonwebtoken');
 const User = require('../model/user.model');
 
 const userLogin = async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
   const userSchema = zod.object({
-    username: zod.string().email(),
+    email: zod.string().email(),
     password: zod.string(),
   });
-  const inputValidation = userSchema.safeParse({ username, password });
+  const inputValidation = userSchema.safeParse({ email, password });
 
   if (!inputValidation.success) {
     console.log('wrong input');
-    res.status(402).json({ message: 'input error' });
-  } else {
-    console.log('correct input');
+    res.status(400).json({ message: 'Invalid email input' });
   }
 
   try {
-    const user = await User.find({ username });
-    if (user.username === username) {
-      var token = jwt.sign({ username: username }, process.env.JWT_SECRET);
-      return res.status(200).json({ token, message: 'success' });
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      console.log('User not found');
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    if (user.email === email && user.password === password) {
+      var token = jwt.sign({ email: email }, process.env.JWT_SECRET);
+      return res.status(200).json({ token, message: 'Login success' });
+    } else {
+      throw new Error();
     }
   } catch (error) {
-    res.status(403).json({ message: 'User login failed' });
+    res.status(403).json({ message: 'Invalid credentials' });
   }
 };
 
-module.exports = { userLogin };
+const requireAuthentication = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    console.log('Token is missing');
+  } else {
+    const tokenWithoutBearer = token.startsWith('Bearer ')
+      ? token.slice(7)
+      : token;
+
+    try {
+      const verify = jwt.verify(tokenWithoutBearer, process.env.JWT_SECRET);
+      req.user = verify;
+      next();
+    } catch (err) {
+      console.log('Invalid token');
+    }
+  }
+};
+
+const userVerify = async (req, res) => {
+  res.status(200).json({ message: 'Token is valid', user: req.user.email });
+};
+
+module.exports = { userLogin, requireAuthentication, userVerify };
--- server/routes/endpoints.js
+++ server/routes/endpoints.js
@@ -2,10 +2,11 @@ const router = require('express').Router();
 
 const UserAuthController = require('../controller/userAuth.controller');
 
-router.get('/', (req, res) => {
-  res.send('100xDiscuss server says Hi');
-});
-
 router.post('/signin', UserAuthController.userLogin);
+router.post(
+  '/',
+  UserAuthController.requireAuthentication,
+  UserAuthController.userVerify
+);
 
 module.exports = router;
